201865b7fa675ecddf76dc43afc7e4151fb4e152
Bartek306 <shwdp13@wp.pl>
2021-10-21 12:56:47 +0200

    Update gitignore

--- REV/api/urls.py
+++ REV/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import reverse, home
+from .views import check_sum, home
 
 urlpatterns = [
     path('', home),
-    path('check_sum', reverse)
+    path('check_sum', check_sum)
 ]
